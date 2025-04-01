52a26f48ad6ace97fb063bde32e92974b4274ba1
Alysson <alyssondavi.16@gmail.com>
2025-03-31 23:16:22 -0300

    desafioxadrez concluido!

--- desafioxadrez.c
+++ desafioxadrez.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+
 int main(){
+// Mover a Torre 5 casas a direita
+printf("Abaixo estara o movimento da Torre:\n");
+for(int i = 0; i < 5; 
+i++) // Imcrementa o contador a cada repetição
+{
+    printf("Direita\n"); // Imprimindo a direção da Torre
+}
 
-    int i = 1;
+ printf("\nAbaixo estara o movimento da Rainha:\n");   
+// Mover a Rainha 8 casas para a esquerda
+int i = 0;
 
-    while(i <= 100)
-    {
-        printf("%d\n", i);
-        i++;
-    }
+while (i < 8){
+    printf("Esquerda\n", i); 
+    i++; // Imcrementa o contador a cada repetição
+}
+
+printf("\nAbaixo estara o movimento do Bispo:\n");
+int l = 0; // Inicializa o contador
+
+    do {
+        printf("Cima\n");
+        printf("Direita\n");
+        l++; // Incrementa o contador a cada repetição
+    } while (l < 5); // Repete até atingir 5 vezes
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
+
 
     
-    return 0;
-}
+    
+        
+
